39d641d240492485a830b0d1511d989f2bdac00e
duynd-lucivn <104339279+duynd-lucivn@users.noreply.github.com>
2024-03-28 09:16:23 +0700

    fix: add introduce page

--- src/views/Settings/Area/DetailArea.tsx
+++ src/views/Settings/Area/DetailArea.tsx
@@ -3,7 +3,7 @@ import { FAKE_DATA, ListIconImage } from '@/utils/constant';
 import { getCollapseCss } from '@/views/Home/style';
 import { RightOutlined } from '@ant-design/icons';
 import type { CollapseProps } from 'antd';
-import { Button, Collapse, List, Popconfirm, Typography, message } from 'antd';
+import { Button, Col, Collapse, List, Popconfirm, Row, Typography, message } from 'antd';
 import type { DataNode } from 'antd/es/tree';
 import { useCallback, useState } from 'react';
 import CardDev from './components/Card';
@@ -136,41 +136,118 @@ const DetailArea = ({
     setOpen(false);
   };
 
-  return (
-    data && (
-      <div css={getCollapseCss()}>
-        <Collapse
-          {...CollapseProp}
-          defaultActiveKey={collapseArray.map((item) => `collapseanchor${item}`)}
-          ghost
-          items={generateCollapseItems(
-            findNodesWithTitle(anchorItems2, data?.titleWithNoIcon ?? '', keyChose, []),
-          )}
-        />
-        <DrawerControl icon={selectedIcon} device={selectedDevice} onClose={onClose} />
-        <Popconfirm
-          title="Delete the area"
-          description="Are you sure to delete this area?"
-          onConfirm={confirm}
-          onCancel={cancel}
-          okText="Yes"
-          cancelText="No"
-        >
-          <Button danger className="absolute top-2 right-4">
-            Xóa
-          </Button>
-        </Popconfirm>
-        <Button type="primary" className="absolute top-2 right-20" onClick={() => setOpen(true)}>
-          Chỉnh sửa
+  return data ? (
+    <div css={getCollapseCss()}>
+      <Collapse
+        {...CollapseProp}
+        defaultActiveKey={collapseArray.map((item) => `collapseanchor${item}`)}
+        ghost
+        items={generateCollapseItems(
+          findNodesWithTitle(anchorItems2, data?.titleWithNoIcon ?? '', keyChose, []),
+        )}
+      />
+      <DrawerControl icon={selectedIcon} device={selectedDevice} onClose={onClose} />
+      <Popconfirm
+        title="Delete the area"
+        description="Are you sure to delete this area?"
+        onConfirm={confirm}
+        onCancel={cancel}
+        okText="Yes"
+        cancelText="No"
+      >
+        <Button danger className="absolute top-2 right-4">
+          Xóa
         </Button>
-        <EditAreaFormModal
-          open={open}
-          onCreate={onCreate}
-          onCancel={() => setOpen(false)}
-          initialValues={abc}
-        />
-      </div>
-    )
+      </Popconfirm>
+      <Button type="primary" className="absolute top-2 right-20" onClick={() => setOpen(true)}>
+        Chỉnh sửa
+      </Button>
+      <EditAreaFormModal
+        open={open}
+        onCreate={onCreate}
+        onCancel={() => setOpen(false)}
+        initialValues={abc}
+      />
+    </div>
+  ) : (
+    <div className="p-4 m-4">
+      <Row className="flex justify-between">
+        <Typography.Title>Thông tin chung</Typography.Title>
+      </Row>
+      <Row gutter={24} style={{ marginTop: 16 }}>
+        <Col xl={8} lg={7} md={24} style={{ paddingRight: 24 }}>
+          <img
+            style={{ width: '100%', height: '100%' }}
+            src={
+              'https://luci.vn/wp-content/uploads/2024/02/png-clipart-smart-city-innovation-business-urban-design-city-building-city-removebg-preview.png.webp'
+            }
+          />
+        </Col>
+        <Col xl={16} lg={17} md={24}>
+          <Row className="mb-4 text-">
+            <Col span={4}>Tên dự án:</Col>
+            <Col offset={1} span={19} style={{ fontWeight: 'bold' }}>
+              Luci IBMS
+            </Col>
+          </Row>
+          <Row className="mb-4">
+            <Col span={4}>Tên miền:</Col>
+            <Col offset={1} span={19} style={{ fontWeight: 'bold' }}>
+              https://luci.vn/
+            </Col>
+          </Row>
+          <Row className="mb-4">
+            <Col span={4}>Thành phố:</Col>
+            <Col offset={1} span={19} style={{ fontWeight: 'bold' }}>
+              Tp. Hà Nội
+            </Col>
+          </Row>
+          <Row className="mb-4">
+            <Col span={4}>Địa chỉ:</Col>
+            <Col offset={1} span={19} style={{ fontWeight: 'bold' }}>
+              Tầng 2, toà New Skyline, Đường Nguyễn Khuyến, Phường Văn Quán, Quận Hà Đông,
+            </Col>
+          </Row>
+          <Row className="mb-4">
+            <Col span={4}>Giới thiệu:</Col>
+            <Col offset={1} span={19} style={{ whiteSpace: 'pre-wrap' }}>
+              Luci iBMS là một hệ thống đồng bộ, cho phép quản lý, điều khiển các hệ thống cơ điện,
+              cung cấp nước sinh hoạt, điều hòa thông gió, cảnh báo môi trường, an ninh, báo cháy
+              trong một tòa nhà…đảm bảo cho việc vận hành các thiết bị trong toàn được chính xác,
+              kịp thời, hiệu quả, tiết kiệm năng lượng.
+              <Col style={{ whiteSpace: 'pre-wrap', marginTop: 16 }}>
+                Bằng cách tích hợp với các thiết bị IoT, hệ thống cho phép trao đổi và liên lạc dữ
+                liệu theo thời gian thực giữa các thành phần khác nhau của tòa nhà. Sự tích hợp liền
+                mạch này giúp nâng cao hiệu quả tổng thể và khả năng đáp ứng của các hoạt động xây
+                dựng.
+              </Col>
+            </Col>
+          </Row>
+          <Row className="mb-4">
+            <Col span={4}>Email:</Col>
+            <Col offset={1} span={19} style={{ fontWeight: 'bold' }}>
+              info@luci.vn
+            </Col>
+          </Row>
+          <Row className="mb-4">
+            <Col span={4}>Hotline:</Col>
+            <Col span={19} offset={1} style={{ fontWeight: 'bold' }}>
+              <Typography.Paragraph
+                ellipsis={{ rows: 1 }}
+                code
+                css={{
+                  [':where(.css-dev-only-do-not-override-15v93vd).ant-typography code']: {
+                    margin: 0,
+                  },
+                }}
+              >
+                0888 729 119
+              </Typography.Paragraph>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
